18fa03816b39c247f4cbe1a5798458b30fdb9a30
jvrjsanchez <javiersanchez.cna@gmail.com>
2024-04-20 12:53:34 -0500

    Updates

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,10 +1,14 @@
 import { useParams } from "react-router-dom";
+import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import fetchPet from "../fetchPet";
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
+import Modal from  './Modal';
 
 
 const Details = () => {
+    const [showModal, setShowModal] = useState(false);
     const { id } = useParams();
     const results = useQuery(["details", id], fetchPet);
 
@@ -25,13 +29,33 @@ const Details = () => {
           <h1>{pet.name}</h1>
           <h2>
             {pet.animal} - {pet.breed} - {pet.city}, {pet.state}
-          <button>Adopt {pet.name}</button>
+          <button onClick={() => setShowModal(true)}>Adopt {pet.name}</button>
           <p>{pet.description}</p>
+          {
+            showModal ?
+            (
+              <Modal>
+                <div>
+                  <h1>Would you like to adopt {pet.name}?</h1>
+                  <div className="buttons">
+                    <button>Yes</button>
+                    <button onClick={() => setShowModal(false)}>No</button>
+                  </div>
+                </div>
+              </Modal>
+            ) : null}
           </h2>
         </div>
       </div>
     );
 };
 
+function DetailsErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <Details {...props}/>
+    </ErrorBoundary>
+  )
+}
   
-  export default Details;
+  export default DetailsErrorBoundary;
